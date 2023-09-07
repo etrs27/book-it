c36aee53de771bdc453aee55a042050309fa0902
Theodore Stansbury <tstansbu.27@gmail.com>
2023-09-07 01:54:22 -0400

    Added account page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import IndexPage from "./components/IndexPage"
 import LoginPage from "./components/LoginPage"
 import RegisterPage from "./components/RegisterPage"
+import AccountPage from "./components/AccountPage"
 import Layout from "./Layout"
 import axios from "axios"
 import { UserContextProvider } from "./UserContext"
@@ -18,6 +19,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
